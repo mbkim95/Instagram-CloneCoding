737796b0745f00d035941627436bf50792ed9298
mbkim95 <mbkim95@naver.com>
2020-02-27 02:11:45 +0900

    Implement following system

--- app/src/main/java/com/project/instagram/Navigation/UserFragment.kt
+++ app/src/main/java/com/project/instagram/Navigation/UserFragment.kt
@@ -1,12 +1,14 @@
 package com.project.instagram.Navigation
 
 import android.content.Intent
+import android.graphics.PorterDuff
 import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import android.widget.ImageView
 import androidx.appcompat.widget.LinearLayoutCompat
+import androidx.core.content.ContextCompat
 import androidx.fragment.app.Fragment
 import androidx.recyclerview.widget.GridLayoutManager
 import androidx.recyclerview.widget.RecyclerView
@@ -17,6 +19,7 @@ import com.google.firebase.firestore.FirebaseFirestore
 import com.project.instagram.LoginActivity
 import com.project.instagram.MainActivity
 import com.project.instagram.Navigation.model.ContentDTO
+import com.project.instagram.Navigation.model.FollowDTO
 import com.project.instagram.R
 import kotlinx.android.synthetic.main.activity_main.*
 import kotlinx.android.synthetic.main.fragment_user.*
@@ -64,6 +67,9 @@ class UserFragment : Fragment() {
             mainActivity.toolbar_title_image?.visibility = View.GONE
             mainActivity.toolbar_username?.visibility = View.VISIBLE
             mainActivity.toolbar_button_back?.visibility = View.VISIBLE
+            fragmentView?.account_button_follow_singout?.setOnClickListener {
+                requestFollow()
+            }
         }
 
         fragmentView?.account_recyclerView?.adapter = UserFragmentRecyclerViewAdapter()
@@ -75,6 +81,7 @@ class UserFragment : Fragment() {
             activity?.startActivityForResult(photoPickerIntent, PICK_PROFILE_FROM_ALBUM)
         }
         getProfileImage()
+        getFollowerAndFollowing()
         return fragmentView
     }
 
@@ -92,6 +99,93 @@ class UserFragment : Fragment() {
                 }
             }
     }
+
+    fun getFollowerAndFollowing() {
+        firestore?.collection("users")?.document(uid!!)
+            ?.addSnapshotListener { documentSnapshot, firebaseFirestoreException ->
+                if (documentSnapshot == null) {
+                    return@addSnapshotListener
+                }
+                var followDTO = documentSnapshot.toObject(FollowDTO::class.java)
+
+                if (followDTO?.followingCount != null) {
+                    fragmentView?.account_textView_following_count?.text =
+                        followDTO.followingCount.toString()
+                }
+                if (followDTO?.followingCount != null) {
+                    fragmentView?.account_textView_follower_count?.text =
+                        followDTO.followerCount.toString()
+                    if (followDTO.followers.containsKey(currentUid!!)) {
+                        fragmentView?.account_button_follow_singout?.text =
+                            getString(R.string.follow_cancel)
+                        fragmentView?.account_button_follow_singout?.background?.setColorFilter(
+                            ContextCompat.getColor(activity!!, R.color.colorLightGray),
+                            PorterDuff.Mode.MULTIPLY
+                        )
+                    } else {
+                        if (uid != currentUid) {
+                            fragmentView?.account_button_follow_singout?.text =
+                                getString(R.string.follow)
+                            fragmentView?.account_button_follow_singout?.background?.colorFilter =
+                                null
+                        }
+                    }
+                }
+            }
+    }
+
+    fun requestFollow() {
+        // Save data to my account
+        val tsDocFollowing = firestore?.collection("users")?.document(currentUid!!)
+        firestore?.runTransaction { transaction ->
+            var followDTO = transaction.get(tsDocFollowing!!).toObject(FollowDTO::class.java)
+            if (followDTO == null) {
+                followDTO = FollowDTO()
+                followDTO.followingCount = 1
+                followDTO.followings[uid!!] = true
+
+                transaction.set(tsDocFollowing, followDTO)
+                return@runTransaction
+            }
+
+            if (followDTO.followings.containsKey(uid)) {
+                // It remove following third person when a third person follow me
+                followDTO.followingCount -= 1
+                followDTO.followings.remove(uid)
+            } else {
+                // It add following third person when a third person don't follow me
+                followDTO.followingCount += 1
+                followDTO.followings.put(uid!!, true)
+            }
+            transaction.set(tsDocFollowing, followDTO)
+            return@runTransaction
+        }
+        // Save data to third person
+        var tsDocFollower = firestore?.collection("users")?.document(uid!!)
+        firestore?.runTransaction { transaction ->
+            var followDTO = transaction.get(tsDocFollower!!).toObject(FollowDTO::class.java)
+            if (followDTO == null) {
+                followDTO = FollowDTO()
+                followDTO!!.followerCount = 1
+                followDTO!!.followers[currentUid!!] = true
+
+                transaction.set(tsDocFollower, followDTO!!)
+                return@runTransaction
+            }
+            if (followDTO!!.followers.containsKey(currentUid)) {
+                // It cancel my follower when I follow a third person
+                followDTO!!.followerCount -= 1
+                followDTO!!.followers.remove(currentUid)
+            } else {
+                // It add my follower when I follow a third person
+                followDTO!!.followerCount += 1
+                followDTO!!.followers[currentUid!!] = true
+            }
+            transaction.set(tsDocFollower, followDTO!!)
+            return@runTransaction
+        }
+    }
+
 
     inner class UserFragmentRecyclerViewAdapter : RecyclerView.Adapter<RecyclerView.ViewHolder>() {
         var contentDTOs: ArrayList<ContentDTO> = arrayListOf()
